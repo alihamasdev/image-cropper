c220856ff58e68498803ab3ae03b9bd2fa0b9ccc
alihamasdev <alihamasdev@gmail.com>
2025-06-21 00:37:25 +0500

    feat: add theme toggle button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import { Crop } from "@/components/crop";
+import { ThemeToggle } from "@/components/theme-button";
 
 export default function HomePage() {
 	return (
-		<main className="container mx-auto min-h-dvh w-full px-5 pt-10 pb-30">
-			<h1 className="mb-12 text-center text-4xl font-bold">Image Cropper</h1>
-			<Crop />
+		<main className="relative container mx-auto min-h-dvh w-full px-5 pt-10 pb-30">
+			<header className="flex w-full items-center justify-between">
+				<h1 className="text-center text-4xl font-bold tracking-tight text-balance">Image Cropper</h1>
+				<ThemeToggle />
+			</header>
+			<div className="mt-12 w-full">
+				<Crop />
+			</div>
 		</main>
 	);
 }
