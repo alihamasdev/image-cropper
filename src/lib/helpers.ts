6f5c186a923be299793b3b23b3dee28d1b159c3e
ScriptSquirrel216 <alihamasdev@gmail.com>
2026-06-09 14:46:52 +0500

    feat: add 3:4 aspect ratio option to helper constants

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -42,6 +42,7 @@ export const aspectRatioList = [
 	{ label: "1 : 1", value: 1 / 1 },
 	{ label: "2 : 3", value: 2 / 3 },
 	{ label: "3 : 2", value: 3 / 2 },
+	{ label: "3 : 4", value: 3 / 4 },
 	{ label: "4 : 3", value: 4 / 3 },
 	{ label: "9 : 16", value: 9 / 16 },
 	{ label: "16 : 9", value: 16 / 9 },
